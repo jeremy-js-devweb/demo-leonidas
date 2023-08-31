303419a29dd69149e2b2940024f4b24677c7fe2e
jeremy-js-devweb <jeremy.js.devweb@gmail.com>
2023-08-31 07:26:04 +0200

    badge ok

--- apps/demo-leonidas/src/app/app.component.ts
+++ apps/demo-leonidas/src/app/app.component.ts
@@ -1,16 +1,23 @@
 import {Component} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {AspectRatioComponent} from "./aspect-ratio/aspect-ratio.component";
+import { HlmBadgeDirective } from '@spartan-ng/badge-helm';
+
 
 @Component({
   standalone: true,
-  imports: [RouterModule, AspectRatioComponent],
+  imports: [RouterModule, AspectRatioComponent, HlmBadgeDirective],
   selector: 'demo-leonidas-root',
   template: `
     <div class="flex flex-col items-center gap-y-3">
       <h1>TEST SPARTAN UI</h1>
       <router-outlet></router-outlet>
       <demo-leonidas-aspect-ratio/>
+
+
+
+      <a target="_blank" href="https://github.com/goetzrobin/spartan" hlmBadge>This is madness. This is spartan.</a>
+
     </div>
   `
 })
