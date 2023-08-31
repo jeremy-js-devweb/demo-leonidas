8b499283017a6986f9f5a535f8b3fce8a64146f9
jeremy-js-devweb <jeremy.js.devweb@gmail.com>
2023-08-31 07:29:26 +0200

    card ok

--- apps/demo-leonidas/src/app/app.component.ts
+++ apps/demo-leonidas/src/app/app.component.ts
@@ -1,12 +1,26 @@
 import {Component} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {AspectRatioComponent} from "./aspect-ratio/aspect-ratio.component";
-import { HlmBadgeDirective } from '@spartan-ng/badge-helm';
-
+import {HlmBadgeDirective} from '@spartan-ng/badge-helm';
+import {
+  HlmCardContentDirective,
+  HlmCardDescriptionDirective,
+  HlmCardDirective,
+  HlmCardFooterDirective,
+  HlmCardHeaderDirective,
+  HlmCardTitleDirective,
+} from '@spartan-ng/card-helm';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, AspectRatioComponent, HlmBadgeDirective],
+  imports: [RouterModule, AspectRatioComponent, HlmBadgeDirective,
+    HlmCardDirective,
+    HlmCardHeaderDirective,
+    HlmCardTitleDirective,
+    HlmCardDescriptionDirective,
+    HlmCardContentDirective,
+    HlmCardFooterDirective,
+  ],
   selector: 'demo-leonidas-root',
   template: `
     <div class="flex flex-col items-center gap-y-3">
@@ -15,8 +29,17 @@ import { HlmBadgeDirective } from '@spartan-ng/badge-helm';
       <demo-leonidas-aspect-ratio/>
 
 
-
       <a target="_blank" href="https://github.com/goetzrobin/spartan" hlmBadge>This is madness. This is spartan.</a>
+
+
+      <section hlmCard>
+        <div hlmCardHeader>
+          <h3 hlmCardTitle>Card Title</h3>
+          <p hlmCardDescription>Card Description</p>
+        </div>
+        <p hlmCardContent>Card Content</p>
+        <p hlmCardFooter>Card Footer</p>
+      </section>
 
     </div>
   `
