8b6d2ff78507b194c23cd7a4acbec06d3647057f
jeremy-js-devweb <jeremy.js.devweb@gmail.com>
2023-08-31 16:02:14 +0200

    feat: popover ok but only with angular sdk 16.1.5 because of ui brain dialog

--- apps/demo-leonidas/src/app/app.component.ts
+++ apps/demo-leonidas/src/app/app.component.ts
@@ -13,6 +13,7 @@ import {
 import {MenuComponent} from "./menu/menu.component";
 import { HlmInputDirective } from '@spartan-ng/input-helm';
 import { HlmLabelDirective } from '@spartan-ng/label-helm';
+import {PopoverComponent} from "./popover/popover.component";
 
 @Component({
   standalone: true,
@@ -24,37 +25,39 @@ import { HlmLabelDirective } from '@spartan-ng/label-helm';
     HlmCardContentDirective,
     HlmCardFooterDirective, MenuComponent,
     HlmInputDirective,
-    HlmLabelDirective
+    HlmLabelDirective, PopoverComponent
   ],
   selector: 'demo-leonidas-root',
   template: `
-    <div class="flex flex-col items-center gap-y-3">
-      <h1>TEST SPARTAN UI</h1>
-      <router-outlet></router-outlet>
-      <demo-leonidas-aspect-ratio/>
+      <div class="flex flex-col items-center gap-y-3">
+          <h1>TEST SPARTAN UI</h1>
+          <router-outlet></router-outlet>
+          <demo-leonidas-aspect-ratio/>
 
 
-      <a target="_blank" href="https://github.com/goetzrobin/spartan" hlmBadge>This is madness. This is spartan.</a>
+          <a target="_blank" href="https://github.com/goetzrobin/spartan" hlmBadge>This is madness. This is spartan.</a>
 
 
-      <section hlmCard>
-        <div hlmCardHeader>
-          <h3 hlmCardTitle>Card Title</h3>
-          <p hlmCardDescription>Card Description</p>
-        </div>
-        <p hlmCardContent>Card Content</p>
-        <p hlmCardFooter>Card Footer</p>
-      </section>
+          <section hlmCard>
+              <div hlmCardHeader>
+                  <h3 hlmCardTitle>Card Title</h3>
+                  <p hlmCardDescription>Card Description</p>
+              </div>
+              <p hlmCardContent>Card Content</p>
+              <p hlmCardFooter>Card Footer</p>
+          </section>
 
 
-      <demo-leonidas-dropdown-menu/>
+          <demo-leonidas-dropdown-menu/>
 
-<!--      <input class="w-80" hlmInput placeholder='Email' type='email' />-->
-      <label hlmLabel>E-Mail
-        <input class='w-80' hlmInput type='email' placeholder='Email'/>
-      </label>
+          <!--      <input class="w-80" hlmInput placeholder='Email' type='email' />-->
+          <label hlmLabel>E-Mail
+              <input class='w-80' hlmInput type='email' placeholder='Email'/>
+          </label>
 
-    </div>
+          <demo-leonidas-popover/>
+
+      </div>
   `
 })
 export class AppComponent {
