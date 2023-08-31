e2b69cdbfcd7165ee0e363bca6a92b249800d88d
jeremy-js-devweb <jeremy.js.devweb@gmail.com>
2023-08-31 17:47:48 +0200

    feat: scroll area ok and separator ok but with this only with angular sdk 16.1.5

--- apps/demo-leonidas/src/app/app.component.ts
+++ apps/demo-leonidas/src/app/app.component.ts
@@ -17,6 +17,10 @@ import {PopoverComponent} from "./popover/popover.component";
 import {BrnProgressComponent, BrnProgressIndicatorComponent,} from '@spartan-ng/ui-progress-brain';
 import {HlmProgressDirective, HlmProgressIndicatorDirective} from '@spartan-ng/progress-helm';
 
+import { NgFor } from '@angular/common';
+import { HlmScrollAreaComponent } from '@spartan-ng/scrollarea-helm';
+import { HlmSeparatorDirective } from '@spartan-ng/separator-helm';
+
 @Component({
   standalone: true,
   imports: [RouterModule, AspectRatioComponent, HlmBadgeDirective,
@@ -28,49 +32,62 @@ import {HlmProgressDirective, HlmProgressIndicatorDirective} from '@spartan-ng/p
     HlmCardFooterDirective, MenuComponent,
     HlmInputDirective,
     HlmLabelDirective, PopoverComponent,
-    BrnProgressComponent, BrnProgressIndicatorComponent, HlmProgressIndicatorDirective, HlmProgressDirective
+    BrnProgressComponent, BrnProgressIndicatorComponent, HlmProgressIndicatorDirective, HlmProgressDirective,
+    NgFor, HlmSeparatorDirective, HlmScrollAreaComponent
   ],
   selector: 'demo-leonidas-root',
   template: `
-    <div class="flex flex-col items-center gap-y-3">
-      <h1>TEST SPARTAN UI</h1>
-      <router-outlet></router-outlet>
-      <demo-leonidas-aspect-ratio/>
+      <div class="flex flex-col items-center gap-y-3">
+          <h1>TEST SPARTAN UI</h1>
+          <router-outlet></router-outlet>
+          <demo-leonidas-aspect-ratio/>
 
 
-      <a target="_blank" href="https://github.com/goetzrobin/spartan" hlmBadge>This is madness. This is spartan.</a>
+          <a target="_blank" href="https://github.com/goetzrobin/spartan" hlmBadge>This is madness. This is spartan.</a>
 
 
-      <section hlmCard>
-        <div hlmCardHeader>
-          <h3 hlmCardTitle>Card Title</h3>
-          <p hlmCardDescription>Card Description</p>
-        </div>
-        <p hlmCardContent>Card Content</p>
-        <p hlmCardFooter>Card Footer</p>
-      </section>
+          <section hlmCard>
+              <div hlmCardHeader>
+                  <h3 hlmCardTitle>Card Title</h3>
+                  <p hlmCardDescription>Card Description</p>
+              </div>
+              <p hlmCardContent>Card Content</p>
+              <p hlmCardFooter>Card Footer</p>
+          </section>
 
 
-      <demo-leonidas-dropdown-menu/>
+          <demo-leonidas-dropdown-menu/>
 
-      <!--      <input class="w-80" hlmInput placeholder='Email' type='email' />-->
-      <label hlmLabel>E-Mail
-        <input class='w-80' hlmInput type='email' placeholder='Email'/>
-      </label>
+          <!--      <input class="w-80" hlmInput placeholder='Email' type='email' />-->
+          <label hlmLabel>E-Mail
+              <input class='w-80' hlmInput type='email' placeholder='Email'/>
+          </label>
 
-      <demo-leonidas-popover/>
+          <demo-leonidas-popover/>
 
-      <brn-progress hlm aria-labelledby='loading' class="w-72">
-        <brn-progress-indicator hlm />
-      </brn-progress>
+          <brn-progress hlm aria-labelledby='loading' class="w-72">
+              <brn-progress-indicator hlm/>
+          </brn-progress>
 
-    </div>
+          <hlm-scroll-area class="h-72 w-48 rounded-md border border-border">
+              <div class="p-4">
+                  <h4 class="mb-4 text-sm font-medium leading-none">Tags</h4>
+                  <div class="text-sm" *ngFor="let tag of tags">
+                      {{ tag }}
+                      <div hlmSeparator class="my-2"></div>
+                  </div>
+              </div>
+          </hlm-scroll-area>
+
+      </div>
   `
 })
 export class AppComponent implements OnInit {
   title = 'demo-leonidas';
 
   value = 0;
+
+  tags = Array.from({ length: 50 }).map((_, i, a) => `v1.2.0-beta.${a.length - i}`);
 
   ngOnInit() {
     setTimeout(() => (this.value = 33), 3000);
